dfbfc9c93a3a204befc4bbd31314ba4c8adede13
Jenny Nguyen <nguyen_j6@denison.edu>
2025-12-11 02:50:47 -0500

    Revert "feat(login-signup): add mock pages for log in and sign up (#4)"
    This reverts commit fff795b28ee1c15a8a4bea89fb09a28c98ec2b7d.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { AuthProvider } from "./lib/AuthContext";
 
-createRoot(document.getElementById("root")!).render(
-	<AuthProvider>
-		<App />
-	</AuthProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
